eae92c8ec9a8d4ad5b84042810e9bba5fd42356a
Roy <165288975+Auresol69@users.noreply.github.com>
2024-11-26 16:29:31 +0700

    Update BookingManager.java

--- Quoc_Bao_OOP/BookingManager.java
+++ Quoc_Bao_OOP/BookingManager.java
@@ -1,3 +1,36 @@
-public class BookingManager {
+import java.util.ArrayList;
 
+public class BookingManager {
+    protected ArrayList<Booking> bookings;
+
+    public BookingManager() {
+        bookings = new ArrayList<>();
+    }
+
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+    }
+
+    public void removeBooking(int index) {
+        bookings.remove(index);
+    }
+
+    public void clearBooking() {
+        bookings.clear();
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(ArrayList<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void findBookingByCustomer(Customer cus) {
+        for (Booking booking : bookings) {
+            if (booking.getCus() == cus)
+                System.err.println(booking.getCus());
+        }
+    }
 }
