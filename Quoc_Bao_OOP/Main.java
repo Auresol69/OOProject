491f65aad8ee33fb4453bf2413963d9b28708454
Roy <165288975+Auresol69@users.noreply.github.com>
2024-11-28 13:39:12 +0700

    Add files via upload

---
+++ Quoc_Bao_OOP/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Booking cus1 = new Booking();
+        cus1.setInfo();
+        BookingManager.findBookingByPhoneNumber("123");
+    }
+}
