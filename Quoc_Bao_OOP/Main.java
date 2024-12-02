4addb0acb3f9202c64201ac136b7b3c8de5dce59
Roy <165288975+Auresol69@users.noreply.github.com>
2024-12-02 16:08:20 +0700

    Update Main.java

--- Quoc_Bao_OOP/Main.java
+++ Quoc_Bao_OOP/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Booking cus1 = new Booking();
+        Booking cus2 = new Booking();
         cus1.setInfo();
+        cus2.setInfo();
         BookingManager.findBookingByPhoneNumber("123");
     }
 }
